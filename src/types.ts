8f1bdae291f03b383e323d80b46d018dd20caa46
CROQUET Mickael <a764bw@login.axa>
2024-08-20 15:38:47 +0200

    feat: adding RGAA2.1.1 rules for checking iframe & frame title existence

---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export type LogMessageParams = {
+  element: string;
+  rule: string; // Assuming rule is a string linked by the RGAA
+  ruleLink: string; // Assuming ruleLink is a string linked by the RGAA
+  message: string;
+};
