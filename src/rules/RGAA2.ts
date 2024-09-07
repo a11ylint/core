9b53e2de9f90195bd4ee7e479d1d0c7a4f07c79f
Croquet Mickael <mickael.croquet@laposte.net>
2024-09-07 18:20:41 +0200

    feat: adding RGAA2.2.1 rules for checking iframe & frame title are revelant

--- src/rules/RGAA2.ts
+++ src/rules/RGAA2.ts
@@ -19,6 +19,33 @@ class RGAA2 {
 
     return wrongFrames;
   }
+
+  // eslint-disable-next-line class-methods-use-this
+  public RGA221({ frames, customBannedWords }: { frames: Array<FrameOrIframe>; customBannedWords: Array<string> }) {
+    const wrongFrames: Array<LogMessageParams> = [];
+    const bannedWords = new Set(['frames', 'iframes', 'frame', 'iframe', ...customBannedWords]);
+
+    // check if titles is in banned words
+    frames.forEach(frame => {
+      const hasTitle = frame.hasAttribute('title');
+      // check with a regex
+      if (!hasTitle) {
+        return;
+      }
+
+      const title = frame.getAttribute('title');
+      const isBanned = [...bannedWords].some(bannedWord => new RegExp(bannedWord, 'gi').test(title!) || title === '');
+      if (isBanned) {
+        wrongFrames.push({
+          element: frame.outerHTML,
+          rule: 'RGAA - 2.2.1',
+          ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#2.2.1',
+          message: 'frame & iframe title should be revelant',
+        });
+      }
+    });
+    return wrongFrames;
+  }
 }
 
 export { RGAA2 };
--- tests/Rules/RGAA2.test.ts
+++ tests/Rules/RGAA2.test.ts
@@ -1,7 +1,7 @@
 import { expect, it } from 'vitest';
 import { RGAA2, FrameOrIframe } from '../../src/rules/RGAA2';
 
-const framesAndIframesWithTitle: Array<FrameOrIframe> = [
+const framesAndIframesWithWrongTitle: Array<FrameOrIframe> = [
   Object.assign(document.createElement('frame'), { title: 'Frame 1' }), // with title
   Object.assign(document.createElement('iframe'), { title: 'Iframe 1' }), // with title
   Object.assign(document.createElement('iframe'), { title: 'Iframe 2' }), // with title
@@ -15,10 +15,17 @@ const framesAndIframesWithoutTitle: Array<FrameOrIframe> = [
   Object.assign(document.createElement('iframe')),
 ];
 
+const framesAndIframesWithGreatTitle: Array<FrameOrIframe> = [
+  Object.assign(document.createElement('frame'), { title: 'Interactive maps' }),
+  Object.assign(document.createElement('iframe'), { title: 'Contracts informations' }),
+  Object.assign(document.createElement('iframe'), { title: 'Current location informations' }),
+  Object.assign(document.createElement('frame'), { title: 'A video game' }),
+];
+
 const core = new RGAA2();
 
 it('RGAA2.1 - frame & iframe should have an attribute title', () => {
-  expect(core.RGAA211(framesAndIframesWithTitle)).toHaveLength(0);
+  expect(core.RGAA211([...framesAndIframesWithWrongTitle, ...framesAndIframesWithGreatTitle])).toHaveLength(0);
   expect(core.RGAA211(framesAndIframesWithoutTitle)).toStrictEqual([
     {
       element: '<frame>',
@@ -31,6 +38,58 @@ it('RGAA2.1 - frame & iframe should have an attribute title', () => {
       rule: 'RGAA - 2.1.1',
       ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#2.1.1',
       message: 'frame & iframe should have an attribute title',
+    },
+  ]);
+});
+
+it('RGAA2.2 - frame & iframe title should be revelant', () => {
+  expect(core.RGA221({ frames: framesAndIframesWithWrongTitle, customBannedWords: [] })).toStrictEqual([
+    {
+      element: '<frame title="Frame 1">',
+      message: 'frame & iframe title should be revelant',
+      rule: 'RGAA - 2.2.1',
+      ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#2.2.1',
+    },
+    {
+      element: '<iframe title="Iframe 1"></iframe>',
+      message: 'frame & iframe title should be revelant',
+      rule: 'RGAA - 2.2.1',
+      ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#2.2.1',
+    },
+    {
+      element: '<iframe title="Iframe 2"></iframe>',
+      message: 'frame & iframe title should be revelant',
+      rule: 'RGAA - 2.2.1',
+      ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#2.2.1',
+    },
+    {
+      element: '<frame title="Frame 2">',
+      message: 'frame & iframe title should be revelant',
+      rule: 'RGAA - 2.2.1',
+      ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#2.2.1',
+    },
+    {
+      element: '<iframe title="" name="Iframe 3"></iframe>',
+      message: 'frame & iframe title should be revelant',
+      rule: 'RGAA - 2.2.1',
+      ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#2.2.1',
+    },
+    {
+      element: '<frame title="" name="Iframe 4">',
+      message: 'frame & iframe title should be revelant',
+      rule: 'RGAA - 2.2.1',
+      ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#2.2.1',
+    },
+  ]);
+
+  expect(core.RGA221({ frames: framesAndIframesWithGreatTitle, customBannedWords: [] })).toHaveLength(0);
+
+  expect(core.RGA221({ frames: [...framesAndIframesWithGreatTitle], customBannedWords: ['video'] })).toStrictEqual([
+    {
+      element: '<frame title="A video game">',
+      message: 'frame & iframe title should be revelant',
+      rule: 'RGAA - 2.2.1',
+      ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#2.2.1',
     },
   ]);
 });
