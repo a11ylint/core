cd42ce251c61bc776b4123b1b30f6a71fc83eeca
unknown <mickael.croquet@laposte.net>
2025-04-01 22:43:16 +0200

    feat: adding RGAA8.1,3,5 rules for document type, title, lang

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { RGAA2 } from './rules/RGAA2.js';
+export { RGAA8 } from './rules/RGAA8.js';
---
+++ src/rules/RGAA8.ts
@@ -0,0 +1,67 @@
+import { LogMessageParams } from '../types.js';
+
+export class RGAA8 {
+  // eslint-disable-next-line class-methods-use-this
+  public RGAA81(documents: Array<Document>) {
+    const wrongElement: Array<LogMessageParams> = [];
+    // check doctype is present
+    documents.forEach(doc => {
+      const { doctype } = doc;
+      if (!doctype) {
+        wrongElement.push({
+          element: doc.baseURI,
+          rule: 'RGAA - 8.1.1',
+          ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#8.1',
+          message: 'Document (Page) should have a doctype',
+        });
+      }
+
+      // RGAA 8.1.3 Doctype should be first
+      const doctypeIndex = doc.firstChild?.nodeType === doc.DOCUMENT_TYPE_NODE;
+      if (doctypeIndex) {
+        wrongElement.push({
+          element: doc.baseURI,
+          rule: 'RGAA - 8.1.3',
+          ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#8.1.3',
+          message: 'Document (Page) doctype should be first',
+        });
+      }
+    });
+    return wrongElement;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public RGAA83(documents: Array<Document>) {
+    const wrongElement: Array<LogMessageParams> = [];
+    // check lang in html tag
+    documents.forEach(doc => {
+      const { lang } = doc.documentElement;
+      if (!lang || lang.length <= 0) {
+        wrongElement.push({
+          element: doc.baseURI,
+          rule: 'RGAA - 8.3',
+          ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#8.3',
+          message: 'Document (Page) should have a lang attribute',
+        });
+      }
+    });
+    return wrongElement;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public RGAA85(documents: Array<Document>) {
+    const wrongElement: Array<LogMessageParams> = [];
+    documents.forEach(doc => {
+      if (doc.title.length <= 0) {
+        wrongElement.push({
+          element: doc.baseURI,
+          rule: 'RGAA - 8.5',
+          ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#8.5',
+          message: 'Document (Page) should have a title',
+        });
+      }
+    });
+
+    return wrongElement;
+  }
+}
