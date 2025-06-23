1a5a1733bfeb6f266f12d5ad848132998089e22a
unknown <mickael.croquet@laposte.net>
2025-06-23 17:57:52 +0200

    feat(mode): adding dom and virtual mode

--- src/rules/RGAA1.ts
+++ src/rules/RGAA1.ts
@@ -1,8 +1,43 @@
-import { LogMessageParams } from '../types.js';
+import { LogMessageParams, Mode } from '../types.js';
+
+export type SvgImageAreaProps = {
+  type: 'img' | 'area' | 'svg';
+  alt?: string;
+  title?: string;
+  ariaLabel?: string;
+  ariaLabelledby?: string | null;
+  role?: string;
+  outerHTML: string;
+};
 
 export class RGAA1 {
+  private mode: Mode;
+
+  constructor(mode: Mode) {
+    this.mode = mode;
+  }
+
+  RGAA11(elements: Array<SvgImageAreaProps>): LogMessageParams[]; // For 'playwright' mode
+
+  RGAA11(elements: Array<HTMLImageElement | HTMLAreaElement | SVGSVGElement>): LogMessageParams[]; // For 'browser' mode
+
+  public RGAA11(
+    elements: Array<SvgImageAreaProps | HTMLImageElement | HTMLAreaElement | SVGSVGElement>,
+  ): LogMessageParams[] {
+    switch (this.mode) {
+      case 'dom':
+        return this.RGAA11_dom(elements);
+      case 'virtual':
+        return this.RGAA11_virtual(elements);
+      default:
+        throw new Error(`Unknown mode: ${this.mode}`);
+    }
+  }
+
   // eslint-disable-next-line class-methods-use-this
-  public RGAA11(elements: Array<HTMLElement | SVGElement>) {
+  private RGAA11_dom(
+    elements: Array<SvgImageAreaProps | HTMLImageElement | HTMLAreaElement | SVGSVGElement>,
+  ): LogMessageParams[] {
     const wrongElement: Array<LogMessageParams> = [];
     elements.forEach(el => {
       if (el instanceof HTMLImageElement) {
@@ -41,6 +76,53 @@ export class RGAA1 {
             message: 'SVG element with role img should have aria-label or aria-labelledby',
           });
         }
+      }
+    });
+
+    return wrongElement;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private RGAA11_virtual(
+    elements: Array<SvgImageAreaProps | HTMLImageElement | HTMLAreaElement | SVGSVGElement>,
+  ): LogMessageParams[] {
+    const wrongElement: Array<LogMessageParams> = [];
+    elements.forEach(el => {
+      const element = el as SvgImageAreaProps;
+      const { alt, title, ariaLabel, ariaLabelledby, outerHTML, role, type } = element;
+      switch (type) {
+        case 'img':
+          if (!alt && !title && !ariaLabel && !ariaLabelledby) {
+            wrongElement.push({
+              element: outerHTML,
+              rule: 'RGAA - 1.1.1',
+              ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#1.1.1',
+              message: 'img elements should have alt, aria-label, title or aria-labelledby',
+            });
+          }
+          break;
+        case 'area':
+          if (!alt && !ariaLabel) {
+            wrongElement.push({
+              element: outerHTML,
+              rule: 'RGAA - 1.1.2',
+              ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#1.1.2',
+              message: 'Area element should have alt or aria-label',
+            });
+          }
+          break;
+        case 'svg':
+          if (role === 'img' && !ariaLabel && !ariaLabelledby) {
+            wrongElement.push({
+              element: outerHTML,
+              rule: 'RGAA - 1.1.5',
+              ruleLink: 'https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#1.1.5',
+              message: 'SVG element with role img should have aria-label or aria-labelledby',
+            });
+          }
+          break;
+        default:
+          break;
       }
     });
 
