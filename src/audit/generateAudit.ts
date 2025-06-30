35f8c192775dfd07c3cca2ae5c8ae76caf52e9f8
unknown <mickael.croquet@laposte.net>
2025-06-30 19:15:30 +0200

    feat(Audit): adding an audit system in HTML,JSON and CLI

---
+++ src/audit/generateAudit.ts
@@ -0,0 +1,97 @@
+import Handlebars from 'handlebars';
+import { readFileSync, writeFileSync } from 'fs';
+import { join, dirname as pathDirname } from 'path';
+import { fileURLToPath } from 'url';
+import { LogMessageParams } from 'src/types.js';
+
+export class AuditGenerator {
+  public generateHtmlAudit({
+    results,
+    baseUrl = 'audit',
+  }: {
+    results: Array<{ url: string; result: { [key: string]: Array<LogMessageParams> } }>;
+    baseUrl?: string;
+  }) {
+    console.info(`Generating HTML file test audit for ${baseUrl}`);
+
+    const filename = fileURLToPath(import.meta.url);
+    const dirname = pathDirname(filename);
+
+    const templateHtml = readFileSync(join(dirname, '../..', 'template', 'template.fr_FR.html'), {
+      encoding: 'utf-8',
+    });
+    const css = readFileSync(join(dirname, '../..', 'template', 'template.css'), {
+      encoding: 'utf-8',
+    });
+    const mappedResultsObject = this.mapTemplateObject(results);
+    const generatedTemplate = Handlebars.compile(templateHtml);
+    const generateResult = generatedTemplate({
+      audits: mappedResultsObject,
+      baseUrl,
+      website: baseUrl.replace(/https?:\/\//, ''),
+      style: css,
+    });
+    writeFileSync(baseUrl ? `${baseUrl.replace(/https?:\/\//, '')}.html` : baseUrl, generateResult);
+    console.info(
+      `HTML file test audit generated at ${baseUrl ? `${baseUrl.replace(/https?:\/\//, '')}.html` : baseUrl}`,
+    );
+  }
+
+  // generateAudit function to create a JSON audit
+  public generateJsonAudit({
+    results,
+    baseUrl,
+  }: {
+    results: Array<{ url: string; result: { [key: string]: Array<LogMessageParams> } }>;
+    baseUrl?: string;
+  }) {
+    console.info(`Generating JSON file test audit for ${baseUrl}`);
+    const mappedResultsObject = this.mapTemplateObject(results);
+    const currentBaseUrl = baseUrl?.replace(/https?:\/\//, '') || 'audit';
+    console.info(`JSON file test audit generated at ${baseUrl}`);
+    writeFileSync(`${currentBaseUrl}.json`, JSON.stringify(mappedResultsObject, null, 2));
+    return mappedResultsObject;
+  }
+
+  // generateAudit function to create CLI audit
+  public generateAudit({
+    results,
+  }: {
+    results: Array<{ url: string; result: { [key: string]: Array<LogMessageParams> } }>;
+  }) {
+    console.info('Generating CLI audit');
+    const mappedResultsObject = this.mapTemplateObject(results);
+
+    console.info(mappedResultsObject);
+  }
+
+  private mapTemplateObject(results: Array<{ url: string; result: { [key: string]: Array<LogMessageParams> } }>) {
+    let mappedResultsObject = {};
+    // eslint-disable-next-line no-restricted-syntax
+    for (const result of results) {
+      const mappedResult = Object.entries(result.result).reduce(
+        (acc, [key, value]) => {
+          acc[key] = {
+            message: value[0]?.message || '',
+            ruleLink: value[0]?.ruleLink || '',
+            issues: value.map(issue => ({
+              element: issue.element,
+            })),
+            hasDomElement: value.some(issue => issue.element && issue.element.trim() !== ''),
+          };
+          return acc;
+        },
+        {} as {
+          [key: string]: {
+            message: string;
+            ruleLink: string;
+            issues: Array<{ element: string }>;
+            hasDomElement?: boolean;
+          };
+        },
+      );
+      mappedResultsObject = { ...mappedResultsObject, ...{ [result.url]: { ...mappedResult } } };
+    }
+    return mappedResultsObject;
+  }
+}
