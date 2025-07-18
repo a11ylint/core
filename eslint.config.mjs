998bf8496b8029cb9bb1cd1148ba819b870adf25
pplancq <paul.plancq@outlook.fr>
2025-07-18 19:59:17 +0200

    chore: add unit test file pattern to ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from '@pplancq/eslint-config';
 
 export default defineConfig({
   enableVitest: true,
+  unitTestFiles: ['tests/**/*.{test,spec}.{js,jsx,ts,tsx}'],
   enablePrettier: 'on',
   extendConfig: [
     {
